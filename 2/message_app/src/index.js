22717a2abe87d73c825d4d8b60007426ae59bfdc
[zivan] <ivanzuev@Pronka-new.local>
2022-08-27 14:05:00 +0300

    add DZ_3

--- 2/message_app/src/index.js
+++ 2/message_app/src/index.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { App } from './App';
+import ReactDOM from "react-dom/client";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage, ProfilePage, ChatPage } from "./pages";
+import { Header } from "./components";
+import "./global.css";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const theme = createTheme({
+  // palette: {
+  //   primary: {
+  //     main: "#ff0000",
+  //   },
+  // },
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/chat/*" element={<ChatPage />} />
+        <Route path="*" element={<h1>404</h1>} />
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
